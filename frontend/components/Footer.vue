d756b9fb782110c38493e753ba21009d33326e23
KensukeOta <bump_kensuke_miwa@i.softbank.jp>
2021-10-08 09:26:26 +0900

    ヘッダーとフッターを配置

---
+++ frontend/components/Footer.vue
@@ -0,0 +1,19 @@
+<template>
+  <footer>
+    (c) OtaKensuke
+  </footer>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss" scoped>
+  footer {
+    background: #F2F2F3;
+    text-align: center;
+    color: #aaa;
+  }
+</style>
---
+++ frontend/components/Header.vue
@@ -0,0 +1,26 @@
+<template>
+  <header>
+    <nuxt-link to="/">NuxtBlogApp</nuxt-link>
+    <div>
+      <nuxt-link to="/register">新規登録</nuxt-link>
+      <nuxt-link to="/login">ログイン</nuxt-link>
+    </div>
+  </header>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss" scoped>
+  header {
+    display: flex;
+    justify-content: space-between;
+    border-bottom: 1px solid $themeColor;
+    a {
+      text-decoration: none;
+    }
+  }
+</style>
